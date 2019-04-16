bcba7eded7703b8d999ec6db046116448f352187
Javontay McElroy <dinomightnetwork@gmail.com>
2019-04-16 13:18:13 -0500

    Committing

--- black-hole/src/components/DumpContainer/Dump/index.js
+++ black-hole/src/components/DumpContainer/Dump/index.js
@@ -3,95 +3,90 @@ import { connect } from 'react-redux';
 import { updateNote, deleteNote } from '../../../actions';
 
 class Dump extends React.Component {
-    state = {
-        editing: false,
-        updateNote: {
-            noteInput: '',
-            experationDate: ''
-        }
-    };
-
-    handleInput = e => {
-        this.setState({
-            ...this.state,
-            updateNote: {
-                ...this.state.updateNote,
-                [e.target.name]: e.target.value
-            }
-        });
-    };
-
-    deleteNote = e => {
-        e.preventDefault();
-        this.props.deleteNote(this.props.dump.id)
+  state = {
+    editing: false,
+    updateNote: {
+      noteInput: '',
+      experationDate: ''
     }
+  };
 
-    handeUpdate = e => {
-        e.preventDefault();
-        this.props.note.updateNote(this.state.noteInput, this.state.experationDate);
-    };
+  handleInput = e => {
+    this.setState({
+      ...this.state,
+      updateNote: {
+        ...this.state.updateNote,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
 
-    render() {
-        return (
-            <div className='note'>
-                <button
-                    onClick={() =>
-                        this.setState({
-                            ...this.state,
-                            editing: true
-                        })
-                    }
-                    style={{
-                        display: !this.state.editing ? 'block' : 'none'
-                    }}
-                >
-                    Edit Entry
+  deleteNote = e => {
+    e.preventDefault();
+    this.props.deleteNote(this.props.dump.id);
+  };
+
+  handeUpdate = e => {
+    e.preventDefault();
+    this.props.note.updateNote(this.state.noteInput, this.state.experationDate);
+  };
+
+  render() {
+    return (
+      <div className='note'>
+        <button
+          onClick={() =>
+            this.setState({
+              ...this.state,
+              editing: true
+            })
+          }
+          style={{
+            display: !this.state.editing ? 'block' : 'none'
+          }}
+        >
+          Edit Entry
         </button>
 
-<<<<<<< HEAD
         <form onSubmit={this.handleUpdate}>
           <p className='message'>
-=======
-                <form onSubmit={this.handleUpdate}>
-                    <p className='message'>
-                        Input-Text:
->>>>>>> d8a207a2d4dfc291c4f08b2aaa7f8a53680d11bf
+            Input-Text:
             {this.state.editing ? (
-                            <input
-                                type='text'
-                                name='noteInput'
-                                value={this.state.updateNote.noteInput}
-                                onChange={this.handleInput}
-                            />
-                        ) : (
-                                this.props.dump.message
-                            )}
-                    </p>
-                    <p className='time-left'>
-                        Time left in orbit:{' '}
-                        {this.state.editing ? (
-                            <input
-                                type='date'
-                                name='experationDate'
-                                value={this.state.updateNote.experationDate}
-                                onChange={this.handleInput}
-                            />
-                        ) : (
-                                'DISPLAY EXPERATION DATE HERE'
-                            )}
-                    </p>
-                    <button>Enter</button>
-                    <button onClick={this.deleteNote}>Delete</button>
-                </form>
-            </div>
-        );
-    }
+              <input
+                type='text'
+                name='noteInput'
+                value={this.state.updateNote.noteInput}
+                onChange={this.handleInput}
+              />
+            ) : (
+              this.props.dump.message
+            )}
+          </p>
+          <p className='time-left'>
+            Time left in orbit:{' '}
+            {this.state.editing ? (
+              <input
+                type='date'
+                name='experationDate'
+                value={this.state.updateNote.experationDate}
+                onChange={this.handleInput}
+              />
+            ) : (
+              'DISPLAY EXPERATION DATE HERE'
+            )}
+          </p>
+          <button>Enter</button>
+          <button onClick={this.deleteNote}>Delete</button>
+        </form>
+      </div>
+    );
+  }
 }
 const mapStateToProps = state => {
-    return state;
+  return state;
 };
 
 export default connect(
-    mapStateToProps,
-    { updateNote, deleteNote }
+  mapStateToProps,
+  { updateNote, deleteNote }
 )(Dump);
--- black-hole/src/components/DumpContainer/index.js
+++ black-hole/src/components/DumpContainer/index.js
@@ -1,74 +1,58 @@
-import React from 'react'
+import React from 'react';
 import { connect } from 'react-redux';
-import Dump from './Dump'
-import './Dumps.scss'
-import { fetchingData, addNote } from '../../actions'
+import Dump from './Dump';
+import './Dumps.scss';
+import { fetchingData, addNote } from '../../actions';
 
 import bg from '../../images/bg2.jpg';
 
 class DumpContainer extends React.Component {
-    state = {
-        message: ''
-    }
+  state = {
+    message: ''
+  };
 
-<<<<<<< HEAD
+  componentDidMount() {
+    this.props.fetchingData();
+  }
+
+  handleChanges = e => {
+    this.setState({
+      ...this.state,
+      message: e.target.value
+    });
+  };
+
+  add = e => {
+    e.preventDefault();
+    this.props.addNote(this.state);
+  };
+
   render() {
     return (
-      <>
-        <div className='DumpContainer'>
-          <h1 className='dump-header'>In Orbit</h1>
-          {this.props.dumps.map((dump, id) => (
-            <Dump dump={dump} key={id} className='message' />
-          ))}
-        </div>
-        <img src={bg} alt='' className='background' />
-      </>
+      <div className='DumpContainer'>
+        {this.props.dumps.map((dump, id) => (
+          <Dump dump={dump} key={id} />
+        ))}
+        <form onSubmit={this.add}>
+          <input
+            onChange={this.handleChanges}
+            name='message'
+            value={this.state.message}
+            type='text'
+          />
+        </form>
+      </div>
     );
   }
-=======
-    componentDidMount() {
-        this.props.fetchingData();
-    }
-
-    handleChanges = e => {
-        this.setState({
-            ...this.state,
-            message: e.target.value
-        })
-    }
-
-    add = e => {
-        e.preventDefault()
-        this.props.addNote(this.state)
-    }
-
-    render() {
-        return (
-            <div className='DumpContainer'>
-                {this.props.dumps.map((dump, id) => (
-                    <Dump
-                        dump={dump}
-                        key={id}
-                    />
-                ))}
-                <form onSubmit={this.add}>
-                    <input
-                        onChange={this.handleChanges}
-                        name='message'
-                        value={this.state.message}
-                        type='text'
-                    />
-                </form>
-            </div>
-        )
-    }
->>>>>>> d8a207a2d4dfc291c4f08b2aaa7f8a53680d11bf
 }
 
 const mapStateToProps = state => {
-    return {
-        dumps: state.note.notes
-    }
-}
+  return {
+    dumps: state.note.notes
+  };
+};
 
-export default connect(mapStateToProps, { fetchingData, addNote })(DumpContainer)
+export default connect(
+  mapStateToProps,
+  { fetchingData, addNote }
+)(DumpContainer);
