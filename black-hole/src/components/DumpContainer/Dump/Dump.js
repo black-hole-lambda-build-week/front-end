9958edde5a5e4e2f5496662a670cc6b79656a7a5
brellin <cekakid@gmail.com>
2019-04-17 12:14:52 -0500

    filtered messages

--- black-hole/src/components/DumpContainer/Dump/Dump.js
+++ black-hole/src/components/DumpContainer/Dump/Dump.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 export default props => {
+    const filter = props.dump.message.replace(/([fsc][uh][cni][kt])/ig, '****').replace(/([b][i][t][c][h])/ig, '*****')
+
     return (
         <div className='note' onClick={() => props.toMessage(props.dump.id)}>
-            <p className='message'>{props.dump.message}
+            <p className='message'>{filter}
             </p>
             <p className='time-left'>
                 Time left in orbit: {props.dump.expirationDate}
