815e1d1512b88e8544fb0ab27aa8ff4090b898ee
Javontay McElroy <dinomightnetwork@gmail.com>
2019-04-18 10:44:40 -0500

    Commiting to merge

--- black-hole/src/components/DumpContainer/Dump/Dump.js
+++ black-hole/src/components/DumpContainer/Dump/Dump.js
@@ -1,127 +1,24 @@
 import React from 'react';
-<<<<<<< HEAD
-import { connect } from 'react-redux';
-import { updateNote, deleteNote } from '../../../actions';
-import { NavLink } from 'react-router-dom';
-
-class Dump extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      editing: false,
-      updateNote: {
-        message: ''
-      }
-    };
-  }
-
-  handleInput = e => {
-    this.setState({
-      ...this.state,
-      updateNote: {
-        ...this.state.updateNote,
-        [e.target.name]: e.target.value
-      }
-    });
-  };
-
-  deleteNote = e => {
-    e.preventDefault();
-    this.props.deleteNote(this.props.dump.id);
-  };
-
-  handleUpdate = e => {
-    e.preventDefault();
-    this.props.updateNote(this.props.dump.id, this.state.updateNote);
-    this.setState({
-      ...this.state,
-      editing: false,
-      updateNote: {
-        ...this.state.updateNote,
-        message: ''
-      }
-    });
-  };
-
-  render() {
-    return (
-      <NavLink to={`/${this.props.dump.id}`}>
-        <div className='note'>
-          <button
-            onClick={() =>
-              this.setState({
-                ...this.state,
-                editing: true
-              })
-            }
-            style={{
-              display: !this.state.editing ? 'block' : 'none'
-            }}
-          >
-            Edit Entry
-          </button>
-
-          <form onSubmit={this.handleUpdate}>
-            <p className='message'>
-              {this.state.editing ? (
-                <input
-                  type='text'
-                  name='message'
-                  value={this.state.updateNote.message}
-                  onChange={this.handleInput}
-                />
-              ) : (
-                this.props.dump.message
-              )}
-            </p>
-            <p className='time-left'>
-              Time left in orbit:{' '}
-              {this.state.editing ? (
-                <input
-                  type='date'
-                  name='experationDate'
-                  value={this.state.updateNote.experationDate}
-                  onChange={this.handleInput}
-                />
-              ) : (
-                'DISPLAY EXPERATION DATE HERE'
-              )}
-            </p>
-            <button>Enter</button>
-            <button onClick={this.deleteNote}>Delete</button>
-          </form>
-        </div>
-      </NavLink>
-    );
-  }
-}
-const mapStateToProps = state => {
-  return state;
-};
-
-export default connect(
-  mapStateToProps,
-  { updateNote, deleteNote }
-)(Dump);
-=======
 import moment from 'moment';
 
 export default props => {
-    console.log(props)
-    let filter = props.dump.message.replace(/([fsc][uh][cni][kt])/ig, '****').replace(/([b][i][t][c][h])/ig, '*****'),
-        timeLeft = (originalTime, timeInOrbit) => {
-            return moment(originalTime).add(timeInOrbit, 'days').fromNow(true)
-        }
+  console.log(props);
+  let filter = props.dump.message
+      .replace(/([fsc][uh][cni][kt])/gi, '****')
+      .replace(/([b][i][t][c][h])/gi, '*****'),
+    timeLeft = (originalTime, timeInOrbit) => {
+      return moment(originalTime)
+        .add(timeInOrbit, 'days')
+        .fromNow(true);
+    };
 
-    return (
-
-        <div className='note' onClick={() => props.toMessage(props.dump.id)}>
-            <p className='message'>{filter}
-            </p>
-            <p className='time-left'>
-                Time left in orbit: {timeLeft(props.dump.expirationDate, props.dump.numberOfDays)}
-            </p>
-        </div>
-    );
-}
->>>>>>> c9ec448f26d8bc2709f2228b2bdb8455be4b2c68
+  return (
+    <div className='note' onClick={() => props.toMessage(props.dump.id)}>
+      <p className='message'>{filter}</p>
+      <p className='time-left'>
+        Time left in orbit:{' '}
+        {timeLeft(props.dump.expirationDate, props.dump.numberOfDays)}
+      </p>
+    </div>
+  );
+};
--- black-hole/src/components/DumpContainer/DumpContainer.js
+++ black-hole/src/components/DumpContainer/DumpContainer.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { connect } from 'react-redux';
 import Dump from './Dump';
-import Add from './Add'
-import Message from './Dump/Message'
+import Add from './Add';
+import Message from './Dump/Message';
 import './Dumps.scss';
 import { fetchingData, addNote } from '../../actions';
 
@@ -11,9 +11,12 @@ import bg from '../../images/bg2.jpg';
 import blackhole from '../../images/animator.gif';
 
 class DumpContainer extends React.Component {
-<<<<<<< HEAD
   state = {
-    message: ''
+    message: {
+      id: '',
+      bool: false
+    },
+    bool: false
   };
 
   componentDidMount() {
@@ -27,139 +30,105 @@ class DumpContainer extends React.Component {
     });
   };
 
-  add = e => {
-    e.preventDefault();
-    this.props.addNote(this.state);
+  toMessage = id => {
+    this.setState({
+      ...this.state,
+      message: {
+        id: id,
+        bool: true
+      }
+    });
+  };
+
+  unMessage = () => {
+    this.setState({
+      ...this.state,
+      message: {
+        id: '',
+        bool: false
+      }
+    });
+  };
+
+  unBool = () => {
+    this.setState({
+      ...this.state,
+      bool: false
+    });
   };
 
   render() {
     return (
       <>
         <div className='DumpContainer'>
-          <h1 className='dump-header'>In Orbit</h1>
-
-          {this.props.dumps.map((dump, id) => (
-            <Dump dump={dump} key={id} />
-          ))}
-          <form onSubmit={this.add}>
-            <input
-              onChange={this.handleChanges}
-              name='message'
-              value={this.state.message}
-              type='text'
+          <h1
+            className='dump-header'
+            style={{
+              display: this.state.bool && 'none'
+            }}
+          >
+            In Orbit
+          </h1>
+          <p
+            className='add'
+            onClick={() => {
+              this.state.message.bool
+                ? this.setState({
+                    ...this.state,
+                    message: {
+                      ...this.state.message,
+                      id: '',
+                      bool: false
+                    }
+                  })
+                : this.setState({
+                    ...this.state,
+                    bool: !this.state.bool
+                  });
+            }}
+          >
+            {this.state.bool
+              ? 'Cancel'
+              : this.state.message.bool
+              ? 'Cancel'
+              : '+ Create a message'}
+          </p>
+          {this.state.bool ? (
+            <Add unBool={this.unBool} />
+          ) : this.state.message.bool ? (
+            <Message
+              dump={
+                this.props.dumps[
+                  this.props.dumps.findIndex(
+                    dump => dump.id === this.state.message.id
+                  )
+                ]
+              }
+              unMessage={this.unMessage}
             />
-          </form>
+          ) : (
+            this.props.dumps.map((dump, id) => (
+              <Dump dump={dump} toMessage={this.toMessage} key={id} />
+            ))
+          )}
         </div>
-
-        <img src={bg} alt='' className='background' />
+        <img
+          src={this.state.bool ? blackhole : bg}
+          alt=''
+          className='background'
+        />
       </>
     );
   }
-=======
-    state = {
-        message: {
-            id: '',
-            bool: false
-        },
-        bool: false
-    };
-
-    componentDidMount() {
-        this.props.fetchingData();
-    }
-
-    handleChanges = e => {
-        this.setState({
-            ...this.state,
-            message: e.target.value
-        });
-    };
-
-    toMessage = id => {
-        this.setState({
-            ...this.state,
-            message: {
-                id: id,
-                bool: true
-            }
-        })
-    }
-
-    unMessage = () => {
-        this.setState({
-            ...this.state,
-            message: {
-                id: '',
-                bool: false
-            }
-        })
-    }
-
-    unBool = () => {
-        this.setState({
-            ...this.state,
-            bool: false
-        })
-    }
-
-    render() {
-        return (
-            <>
-                <div className='DumpContainer'>
-                    <h1
-                        className='dump-header'
-                        style={{
-                            display: this.state.bool && 'none'
-                        }}
-                    >In Orbit</h1>
-                    <p
-                        className='add'
-                        onClick={() => {
-                            this.state.message.bool ?
-                                this.setState({
-                                    ...this.state,
-                                    message: {
-                                        ...this.state.message,
-                                        id: '',
-                                        bool: false
-                                    }
-                                }) :
-                                this.setState({
-                                    ...this.state,
-                                    bool: !this.state.bool
-                                })
-                        }}>{this.state.bool ?
-                            'Cancel' :
-                            this.state.message.bool ?
-                                'Cancel' :
-                                '+ Create a message'}</p>
-                    {this.state.bool ?
-                        <Add
-                            unBool={this.unBool}
-                        /> :
-                        this.state.message.bool ?
-                            <Message
-                                dump={this.props.dumps[this.props.dumps.findIndex(dump => dump.id === this.state.message.id)]}
-                                unMessage={this.unMessage}
-                            /> :
-                            this.props.dumps.map((dump, id) => (
-                                <Dump dump={dump} toMessage={this.toMessage} key={id} />
-                            ))}
-                </div>
-                <img src={this.state.bool ? blackhole : bg} alt='' className='background' />
-            </>
-        );
-    }
->>>>>>> c9ec448f26d8bc2709f2228b2bdb8455be4b2c68
 }
 
 const mapStateToProps = state => {
-    return {
-        dumps: state.note.notes
-    };
+  return {
+    dumps: state.note.notes
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    { fetchingData, addNote }
+  mapStateToProps,
+  { fetchingData, addNote }
 )(DumpContainer);
