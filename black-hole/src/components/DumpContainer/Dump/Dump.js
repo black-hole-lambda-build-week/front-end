d4a8519e4cb883f1767231039a6b1599212b7326
brellin <cekakid@gmail.com>
2019-04-18 18:28:32 -0500

    Will not display if time left less than 0

--- black-hole/src/components/DumpContainer/Dump/Dump.js
+++ black-hole/src/components/DumpContainer/Dump/Dump.js
@@ -3,20 +3,18 @@ import moment from 'moment';
 
 export default props => {
   console.log(props);
-  let filter = props.dump.message
-      .replace(/([fsc][uh][cni][kt])/gi, '****')
-      .replace(/([a][s][s])/gi, '***')
-      .replace(/([pd][ia][sm][sn])/gi, '****')
-      .replace(/([bp][ir][ti][c][hk])/gi, '*****'),
+  let curseFilter = props.dump.message
+    .replace(/([fsc][uh][cni][kt])/gi, '****')
+    .replace(/([a][s][s])/gi, '***')
+    .replace(/([pd][ia][sm][sn])/gi, '****')
+    .replace(/([bp][ir][ti][c][hk])/gi, '*****'),
     timeLeft = (originalTime, timeInOrbit) => {
-      return moment(originalTime)
-        .add(timeInOrbit, 'days')
-        .fromNow(true);
+      return moment(originalTime).add(timeInOrbit, 'days').fromNow(true);
     };
 
   return (
     <div className='note' onClick={() => props.toMessage(props.dump.id)}>
-      <p className='message'>{filter}</p>
+      <p className='message'>{curseFilter}</p>
       <p className='time-left'>
         Time left in orbit:{' '}
         {timeLeft(props.dump.expirationDate, props.dump.numberOfDays)}
